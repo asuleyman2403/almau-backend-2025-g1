ee51221b83d2c30ccd4ac29d2aa8669e812dadf3
Assyl Suleiman <asuleyman2403@gmail.com>
2025-03-27 06:34:16 +0500

    LECTURE 10: ER diagram

---
+++ lecture_5/shop/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import get_product_list, get_product_item, delete_product_item, get_main_page, edit_product_item
+
+urlpatterns = [
+    path('', get_main_page, name='index_page'),
+    path('products/', get_product_list, name='products_page'),
+    path('products/<int:pk>/', get_product_item, name='product_details'),
+    path('products/<int:pk>/delete/', delete_product_item, name='remove_product'),
+    path('products/<int:pk>/edit/', edit_product_item, name='edit_product'),
+]
+
